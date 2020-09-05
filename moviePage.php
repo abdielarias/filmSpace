1ccf433f2ed29571d54e19d11a4a53ab616bb49d
Abdiel <abdiel.unlimited@gmail.com>
2020-09-05 13:07:33 -0400

    added rating and certification on moviePage

--- moviePage.php
+++ moviePage.php
@@ -5,10 +5,14 @@ $movieID = $_GET["movieID"];
 
 <div class="moviePage">
   <div class="moviePage-topBanner">
-
+    <div class="moviePage-poster"></div>
+    <div class="moviePage-desc">
+      <div class="moviePage-title"></div>
+      <div class="moviePage-cert"><span id="cert"></span><span id="runtime"></span></div>
+    </div>
   </div>
 
-  <div class="moviePage-bodyDiv">
+  <div class="moviePage-trailerDiv">
     <div class="iframeWrapper">
     </div>
   </div>
@@ -21,9 +25,16 @@ $movieID = $_GET["movieID"];
 
 const API_KEY = "6109ef65464c6279114456237b791d38";
 var topBanner = document.querySelector(".moviePage-topBanner");
-var bodyDiv = document.querySelector(".moviePage-bodyDiv");
+var trailerDiv = document.querySelector(".moviePage-trailerDiv");
 var iframeWrapper = document.querySelector(".iframeWrapper");
+var posterDiv = document.querySelector(".moviePage-poster");
+var descDiv = document.querySelector(".moviePage-desc");
+var titleDiv = document.querySelector(".moviePage-title");
+var movieCertDiv = document.querySelector(".moviePage-cert");
+var runtime = document.querySelector("#runtime");
+
 var movieID = <?php echo $movieID; ?>;
+var movieRating = document.querySelector("#cert");
 const movieURL = "https://api.themoviedb.org/3/movie/"+movieID+"?api_key="+API_KEY+"&language=en-US";
 
 fetch(movieURL)
@@ -35,18 +46,38 @@ fetch(movieURL)
   //movie poster
   var poster = document.createElement("img");
   poster.src = "https://image.tmdb.org/t/p/w780/"+movie.poster_path;
-
-  topBanner.appendChild(poster);
+  posterDiv.appendChild(poster);
 
   //title
   var title = document.createElement("h1");
   title.innerHTML = movie.title+" ("+movie.release_date.split("-")[0]+")";
-  topBanner.appendChild(title);
+  titleDiv.appendChild(title);
+
+  //movie rating
+  fetch("https://api.themoviedb.org/3/movie/"+movieID+"/release_dates?api_key="+API_KEY)
+  .then((response) => response.json())
+  .then((certs) => {
+      movieRating.innerHTML = certs.results[0].release_dates[0].certification;
+  })
+  .catch((err) => console.log(err));
+
+  //movie length
+  runtime.innerHTML = Math.trunc(movie.runtime/60)+" hr "+movie.runtime%60+" min";
 
   //movie description
   var desc = document.createElement("p");
   desc.innerHTML = movie.overview;
-  topBanner.appendChild(desc);
+  descDiv.appendChild(desc);
+
+
+
+
+
+  //movie genres
+
+
+  //movie release date
+
 
   //make request to get a json object with a possible trailer attached. Check if it's youtube, then show trailer with a youtube link
   fetch("https://api.themoviedb.org/3/movie/"+movieID+"/videos?api_key="+API_KEY+"&language=en-US")
@@ -71,6 +102,13 @@ fetch(movieURL)
     }
   });
 
+  //Director(s)
+
+  //Writer(s)
+
+  //stars
+
+
 
 
   //run a new stylesheet for the new elements:
@@ -79,8 +117,6 @@ fetch(movieURL)
   box-sizing: border-box;
   width: 100%;
   padding:20px;
-  grid-row-start: 1;
-  grid-row-end: 3;
 
   `;
 
@@ -90,11 +126,9 @@ fetch(movieURL)
 
   `;
 
-  desc.style = `
+  descDiv.style = `
   box-sizing: border-box;
   font-family: arial;
-  grid-column-start: 2;
-  grid-column-end: 3;
   width: 100%;
   `
 
