a0356cb01248c759f0c59a19d52b7d067f4458c9
Abdiel <abdiel.unlimited@gmail.com>
2020-10-19 18:25:19 -0400

    made writeReviews page responsive

--- index.php
+++ index.php
@@ -28,7 +28,11 @@ include 'header.php';
     document.body.appendChild(script);
 </script>
 
+<br><br>
+<footer style="position:relative;bottom:-200px;width:100%;">
+  <div id="cred">&nbsp;&nbsp;&nbsp;<a href="https://abdielarias.com/">return to portfolio (abdielarias.com)</a><br><br>
+<span style="color: #1c1c1c">This product uses the TMDb API but is not endorsed or certified by TMDb.</span></div><br>
+</footer>
 
-<?php
-include 'footer.php';
-?>
+</body>
+</html>
